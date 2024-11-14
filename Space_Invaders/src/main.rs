04108f65be608e0663222409af27cbdccf1912cb
Maximilian Alexander Deichsel <devatwork.mad@gmail.com>
2024-11-14 12:05:55 +0100

    movement working

--- Space_Invaders/src/main.rs
+++ Space_Invaders/src/main.rs
@@ -1,12 +1,15 @@
 mod game;
+mod systems;
 
 use bevy::prelude::*;
 use crate::game::GamePlugin;
+use crate::systems::spawn_camera;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(GamePlugin)
+        .add_systems(Startup, spawn_camera)
         .run();
 }
 
