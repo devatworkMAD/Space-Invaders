a3ad680e04f51adb40d48568a31b41979ac48af5
devatworkMAD <devatwork.mad@gmail.com>
2024-11-26 15:07:27 +0100

    spawning invader

--- Space_Invaders/src/game/enemy/systems.rs
+++ Space_Invaders/src/game/enemy/systems.rs
@@ -1,6 +1,21 @@
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
+use crate::game::enemy::components::Enemy;
 
-pub fn spawn_enemy(){
+pub fn spawn_enemy(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
     println!("spawning enemy");
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() * 0.5, window.height() * 0.5, 0.0),
+            texture: asset_server.load("invader.png"),
+            ..default()
+        },
+        Enemy {},
+    ));
 }
